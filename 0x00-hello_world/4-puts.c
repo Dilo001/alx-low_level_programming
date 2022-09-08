6d9c4abf462e29a021f4587f54fa5e965f63df53
Adamson Ndem <dilgrand@gmail.com>
2022-09-08 22:57:22 +0000

    C program that prints the exact word

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Description: Print a string using followed by a new line
- * Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
